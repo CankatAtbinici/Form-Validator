6333135e9afce56644a39a5f8ee97c48c29f6ade
Cankat Atbinici <97751358+CankatAtbinici@users.noreply.github.com>
2022-02-05 17:09:57 +0300

    CHECKED SPACES AND EMAİL

--- script.js
+++ script.js
@@ -11,7 +11,54 @@ let register = document.getElementById("register");
 let submit = document.getElementById("submitBtn");
 
 
-//if no data in form area bordercolor going to be red
+
+
+
+form.addEventListener("submit", function (x)  {
+    checkFormElement ([name , lastName , userName , password , repassword , phoneNumber , email]);
+    // passwordCOntrol(password.value , repassword.value);
+    x.preventDefault();
+    emailValidator();
+
+} )
+
+// İS EMAİL REAL VALİDATOR
+//Regexp from : https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript 
+const validateEmail = (stringData) => {
+    return String(stringData)
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+
+
+
+function emailValidator ( ) {
+    if(!validateEmail(email.value) ) {
+        alert("Check your E-mail Adress");
+        email.value = "";
+        email.className = "error"
+    }
+}
+
+
+
+// checking no empty space in form // Used forEach but other method staying comment under the this function
+function checkFormElement(e) {
+    e.forEach(function (input) {
+        if(input.value === ""){
+            input.className = "error";
+            input.setAttribute("placeholder" , "Required Space")
+         }else{
+             input.className = "success";
+           
+         }
+    })
+}
+
+        /*
+//if no data or wrong data in form area = bordercolor going to be red and alert 
+//LONG WAY
 
 function error (i) {
     i.className = "error"
@@ -50,23 +97,35 @@ if(phoneNumber.value === ""){
 }
 if(email.value === ""){
     error(email);
+}else if (!validateEmail(email.value)){
+    alert("Check your E-mail Adress");
 }else{
-    email.className = ""
+    email.className = "";
 }
 
 e.preventDefault();
-})
+}) */
+
+ 
 
 
 
-//Regexp from : https://stackoverflow.com/questions/46155/whats-the-best-way-to-validate-an-email-address-in-javascript 
-const validateEmail = (email) => {
-    return String(email)
-      .toLowerCase()
-      .match(
-        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      );
-  };
+
+
+ /*
+ function passwordCOntrol(first , second) {
+    console.log(first );
+    console.log(second);
+         if ((first) != ( ı )) {
+            console.log("sifreler eşleşmedi");
+       } else if ((first) === (second )){
+           console.log("şifreler eşleşti");
+       }
+ } */
+
+
+
+
 
 
 
